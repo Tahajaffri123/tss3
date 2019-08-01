f18590254ab8533910ae6b0cdbdb77e0535115ff
jamesjoel <james.joelj@gmail.com>
2019-08-01 15:59:19 +0530

    my first commit

---
+++ array1.php
@@ -0,0 +1,14 @@
+<?php
+$data = array();
+$data[]="red";
+$data[]="green";
+$data[]="blue";
+$data[]=500;
+$data[]=75.35;
+$data[]=true;
+
+
+// print_r($data);
+echo $data[3]+$data[4];
+
+?>
---
+++ array2.php
@@ -0,0 +1,14 @@
+<?php
+$data1=array();
+$data1["a"]="red";
+$data1["b"]="green";
+$data1["c"]="blue";
+
+//print_r($data1);
+$data2=array("name"=>"rohit", "age"=>25, "city"=>"indore", "gender"=>"male");
+
+// print_r($data2);
+echo "Your Name : ".$data2["name"];
+
+
+?>
