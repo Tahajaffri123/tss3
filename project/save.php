a5347c2b12cd881c95841d3ec5b6864e7ea66ac3
jamesjoel <james.joelj@gmail.com>
2019-08-03 14:12:04 +0530

    insert in database

--- project/save.php
+++ project/save.php
@@ -1,5 +1,11 @@
 <?php
 // print_r($_POST);
+// Connection Stable
+// $valiable = mysqli_connect(hostname, username, password, dbname)
+// $con = mysqli_connect("localhost", "root", "", "tss3");
+$con = mysqli_connect("localhost", "admin", "admin", "tss3");
+// create database connection
+
 $a = $_POST['f_name'];
 $b = $_POST['username'];
 $c = $_POST['pass'];
@@ -7,4 +13,17 @@ $d = $_POST['contact'];
 $e = $_POST['gender'];
 $f = $_POST['add'];
 $g = $_POST['city'];
+// getting POST data
+
+$query="INSERT INTO user (full_name, username, password, city, address, contact, gender) VALUES ('$a', '$b', '$c', '$g', '$f', '$d', '$e')";
+// make query
+
+
+mysqli_query($con, $query);
+// fire the query
+
+
+header("location:login.php");
+// redirect to the login.php page
+
 ?>
