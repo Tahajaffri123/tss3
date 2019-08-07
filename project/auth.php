c8b94f25ae70779557175d9d75ccd7e10b6d4fac
jamesjoel <james.joelj@gmail.com>
2019-08-07 17:09:31 +0530

    login auth complete

--- project/auth.php
+++ project/auth.php
@@ -5,12 +5,21 @@ $p = $_POST['pass'];
 
 $que = "SELECT * FROM user WHERE username='$u'";
 $result=mysqli_query($con, $que);
+// $result is a object witch can store data base info
+// but this is a non-reable values
 
 if(mysqli_num_rows($result)==1)
 {
+	// convart $result to readble assoc array
 	$data=mysqli_fetch_assoc($result);
+
 	if($data['password']==$p)
 	{
+		$_SESSION['name']=$data['full_name'];
+		$_SESSION['id']=$data['id'];
+		$_SESSION['is_user_logged_in']=true;
+
+
 		header("location:my_account.php");
 	}
 	else
