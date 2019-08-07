edf43013b612aef78e85feca0ea91201c9f71fec
iamdipen <dipendtrasinghpawar@gmail.com>
2019-08-07 17:16:36 +0530

    modified auth,login,my account page

--- mittal/project/auth.php
+++ mittal/project/auth.php
@@ -1,14 +1,16 @@
 <?php
 include("dbinfo.php");
+// print_r($_POST);die;
 $u = $_POST['username'];
-$p = $_POST['Pass'];
+$p = $_POST['pass'];
 
-$que = "SELECT * FROM register WHERE username='$u'";
+echo $que = "SELECT * FROM register WHERE username='$u'";
+// die;
 
 $result = mysqli_query($con,$que);
 if(mysqli_num_rows($result)==1)
 {
-	$data=mysqli_fecth_assoc($result);
+	$data=mysqli_fetch_assoc($result);
 	if($data['password']==$p)
 	{
 		header("location:my_account.php");
--- mittal/project/my_account.php
+++ mittal/project/my_account.php
@@ -4,4 +4,4 @@ include("header.php");
 <p>My Account page </p>
 <?php
 include("footer.php");
-?>
+?>	
