264e190135137f3bb8f5d11a2025de46e8b44f0f
tahajaffri123 <tahajaffri87@gmail.com>
2019-08-29 12:10:47 +0530

    day 3 javascript

--- javascript/day1_c.php
+++ javascript/day1_c.php
@@ -3,19 +3,31 @@
 <head>
 	<title></title>
 	<script type="text/javascript">
-		function show()
+		function show(n)
 		{
-			var a = document.getElementById("f_num").value;			
+
+			var a = document.getElementById("f_num").value;
 			var b = document.getElementById("s_num").value;	
 
 
 			var x = parseInt(a);
 			var y = parseInt(b);
 
-			var c = x+y;
+			switch(n){
+				case 1 : var c = x+y;
+						break;
+				case 2 : var c = x-y;
+						break;
+				case 3 : var c = x/y;
+						break;
+				case 4 : var c = x*y;
+						break;
+			}
+			
 
-			alert("Your Ans is "+c);		
+			document.getElementById("ans").innerHTML="Your ans is "+c;
 		}
+		
 	</script>
 </head>
 <body>
@@ -26,6 +38,10 @@ First Number<input type="text" id="f_num"/>
 Second Number<input type="text" id="s_num"/>
 <Br />
 <Br />
-<button onclick="show()">OK</button>
+<button onclick="show(1)">+</button>
+<button onclick="show(2)">-</button>
+<button onclick="show(3)">/</button>
+<button onclick="show(4)">x</button>
+<h2 id="ans"></h2>
 </body>
 </html>
--- mittal/javascript/task2.php
+++ mittal/javascript/task2.php
@@ -2,9 +2,64 @@
 <html>
 <head>
 	<title></title>
+	<style type="text/css">
+		#div
+		{
+			height: 50px;
+			width: 250px;
+			/*background-color: red;*/
+			border: 2px solid black;
+		}
+	
+		.ancer
+		{
+			 text-decoration: none;
+             display: inline-block;
+             margin: 10px 10px 0px 10px;
+              /*color: white;*/
+             border: 2px solid #e7e7e7;
+             border-radius: 5px;
+             text-decoration : none;
+             padding : 5px;
+             text-align: center;
+             background-color: #e7e7e7;
+             font-weight: bold;
+            
+		}
+	</style>
+	<script type="text/javascript">
+		function a()
+		{
+		   document.getElementById("input").value="a";
+		}
+		function b()
+		{
+		   document.getElementById("input").value="b";
+		}
+		function c()
+		{
+		   document.getElementById("input").value="c";
+		}
+		function d()
+		{
+		   document.getElementById("input").value="d";
+		}
+		function e()
+		{
+		   document.getElementById("input").value="e";
+		}
+	</script>
 </head>
 <body>
-<input type="text" id="input" />
-<div id="div"></div>
+&nbsp; &nbsp; &nbsp; &nbsp; <input type="text" id="input" />
+<br/>
+<br />
+<div id="div">
+	<a class="ancer" href="#" onclick="a()">A</a>
+	<a class="ancer" href="#" onclick="b()">B</a>
+	<a class="ancer" href="#" onclick="c()">C</a>
+	<a class="ancer" href="#" onclick="d()">D</a>
+	<a class="ancer" href="#" onclick="e()">E</a>
+</div>
 </body>
 </html>
