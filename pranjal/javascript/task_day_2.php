4928c02393a4f361b24c8c95e600fdf7a23f0da6
pranjaljaiswal <pranjaljaiswal495@gmail.com>
2019-08-28 23:44:42 -0700

    project files

---
+++ pranjal/javascript/task_day_2.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript">
+		function a()
+		{
+			document.getElementById("input").value="A";
+			// alert()
+		}
+		function b()
+		{
+			document.getElementById("input").value="B";
+		}
+		function c()
+		{
+			document.getElementById("input").value="C";
+		}
+		function d()
+		{
+			document.getElementById("input").value="D";
+		}
+		function e()
+		{
+			document.getElementById("input").value="E";
+		}
+
+
+		
+	</script>
+	<style type="text/css">
+		div{
+			border : 2px solid black;
+			height: 100px;
+			width: 400px
+		}
+	
+		.button{
+    			display: inline-block;
+   			    padding: 20px;
+   			    margin: 10px;
+    			text-align: center;
+    			border:2px solid black;
+    			color: #000;
+    			font-weight: bold;
+				text-decoration-line:none;
+
+}  
+	</style>
+</head>
+<body>
+	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <input type="text" id="input">
+	<br/>
+	<br/>
+	<br/>
+
+	<div id="box">
+		<a class="button" href="#" onclick="a()" >A</a>
+		<a class="button" href="#" onclick="b()" >B</a>
+		<a class="button" href="#" onclick="c()" >C</a>
+		<a class="button" href="#" onclick="d()" >D</a>
+		<a class="button" href="#" onclick="e()" >E</a>
+
+		
+
+	</div>
+
+</body>
+</html>
