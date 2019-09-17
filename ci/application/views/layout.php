1a4ee13324826718ee6b6a5fc1c0ccafd03f18b1
sahu pc <sahusonu208@gmail.com>
2019-09-17 17:39:18 +0530

    my peoject

--- ci/application/views/layout.php
+++ ci/application/views/layout.php
@@ -8,22 +8,22 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <!-- Brand -->
-  <a class="navbar-brand" href="#">Codeingiter</a>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">  
+    <!-- Brand -->
+    <a class="navbar-brand" href="#">Codeingiter</a>
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo site_url("home") ?>">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo site_url("home/about") ?>">About</a>
-      </li>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo site_url("home") ?>">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo site_url("home/about") ?>">About</a>
+        </li>
       <li class="nav-item">
         <a class="nav-link" href="<?php echo site_url("home/contact") ?>">Contact</a>
       </li>
